af1e48e559b45895fd6dd75eaa8aaf067880aabd
Mathias Falkenberg <mathias.falkenberg@wsa.com>
2020-11-25 11:08:31 +0100

    Refactored Gauge control. added reset button.

--- src/components/Climate.tsx
+++ src/components/Climate.tsx
@@ -1,34 +1,60 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect, Component } from 'react'
 import { Sensor } from '../lib/Sensor';
 
+interface GaugeProps {
+  id: "temperature" | "humidity";
+  sensor: Sensor;
+}
+
+interface GaugeState {
+  value: number | null;
+  min: number | null;
+  max: number | null;
+}
+
+const Gauge = (props: GaugeProps) => {
+  const [state, setState] = useState<GaugeState>({ value: null, min: null, max: null });
+
+  useEffect(() => {
+    props.sensor.on(props.id, updateValues)
+
+    return () => {
+      props.sensor.off(props.id, updateValues);
+    }
+  }, [props.sensor, props.id]);
+
+  const updateValues = (value: number) => {
+    setState(({ min, max }) => {
+      return {
+        value: value,
+        min: min ? Math.min(min, value) : value,
+        max: max ? Math.max(max, value) : value
+      }
+    })
+  }
+
+  return <div id={props.id} className='gauge'>
+    <div className="gauge__value">
+      {props.id}: {state.value ?? '-'}
+    </div>
+    <div className="gauge__min">
+      Min: {state.min ?? '-'}
+    </div>
+    <div className="gauge__max">
+      Max: {state.max ?? '-'}
+    </div>
+    <button onClick={() => setState({ ...state, min: state.value, max: state.value})}>Reset</button>
+  </div>
+}
+
 type ClimateProps = { sensor: Sensor };
-type ClimateState = { temperature: number | null, humidity: number | null };
 
-class Climate extends Component<ClimateProps, ClimateState> {
-  state: ClimateState = {
-    temperature: null,
-    humidity: null,
-  };
-
-  componentDidMount() {
-    this.props.sensor.on('temperature', temperature => this.setState({ temperature }));
-    this.props.sensor.on('humidity', humidity => this.setState({ humidity }));
-  }
-
-  componentWillUnmount() {
-    this.props.sensor.clearListeners();
-  }
-
+class Climate extends Component<ClimateProps> {
   render() {
     return (
       <div>
-        <div id="temperature">
-          Temperature: {this.state.temperature ?? '-'}
-        </div>
-
-        <div id="humidity">
-          Humidity: {this.state.humidity ?? '-'}
-        </div>
+        <Gauge id="temperature" sensor={this.props.sensor} />
+        <Gauge id="humidity" sensor={this.props.sensor} />
       </div>
     );
   }
