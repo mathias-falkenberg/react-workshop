499453a4d6ef3d40adf57ac05b56c32ef0299b5e
Mathias Falkenberg <mathias.falkenberg@wsa.com>
2020-11-25 13:15:54 +0100

    Add more styling.

--- src/components/Climate.tsx
+++ src/components/Climate.tsx
@@ -1,60 +1,15 @@
-import React, { useState, useEffect, Component } from 'react'
+import React, { Component } from 'react'
 import { Sensor } from '../lib/Sensor';
-
-interface GaugeProps {
-  id: "temperature" | "humidity";
-  sensor: Sensor;
-}
-
-interface GaugeState {
-  value: number | null;
-  min: number | null;
-  max: number | null;
-}
-
-const Gauge = (props: GaugeProps) => {
-  const [state, setState] = useState<GaugeState>({ value: null, min: null, max: null });
-
-  useEffect(() => {
-    props.sensor.on(props.id, updateValues)
-
-    return () => {
-      props.sensor.off(props.id, updateValues);
-    }
-  }, [props.sensor, props.id]);
-
-  const updateValues = (value: number) => {
-    setState(({ min, max }) => {
-      return {
-        value: value,
-        min: min ? Math.min(min, value) : value,
-        max: max ? Math.max(max, value) : value
-      }
-    })
-  }
-
-  return <div id={props.id} className='gauge'>
-    <div className="gauge__value">
-      {props.id}: {state.value ?? '-'}
-    </div>
-    <div className="gauge__min">
-      Min: {state.min ?? '-'}
-    </div>
-    <div className="gauge__max">
-      Max: {state.max ?? '-'}
-    </div>
-    <button onClick={() => setState({ ...state, min: state.value, max: state.value})}>Reset</button>
-  </div>
-}
+import Gauge from './Gauge';
 
 type ClimateProps = { sensor: Sensor };
 
 class Climate extends Component<ClimateProps> {
   render() {
     return (
-      <div>
-        <Gauge id="temperature" sensor={this.props.sensor} />
-        <Gauge id="humidity" sensor={this.props.sensor} />
+      <div className="climate">
+        <Gauge id="temperature" unit="°C" sensor={this.props.sensor} />
+        <Gauge id="humidity" unit="%" sensor={this.props.sensor} />
       </div>
     );
   }
---
+++ src/components/Gauge.tsx
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react'
+import { Sensor, SensorEvent } from '../lib/Sensor';
+
+interface DisplayProps {
+    id: string;
+    label: string;
+    value: number | null;
+    unit: string;
+}
+
+const Display = (props: DisplayProps) => (
+    <div className={`gauge__${props.id}`}>
+        {props.label}: {props.value?.toFixed(1) ?? '-'} {props.unit}
+    </div>
+)
+
+interface GaugeProps {
+    id: SensorEvent;
+    sensor: Sensor;
+    unit: string;
+}
+
+interface GaugeState {
+    value: number | null;
+    min: number | null;
+    max: number | null;
+}
+
+const Gauge = (props: GaugeProps) => {
+    const [state, setState] = useState<GaugeState>({ value: null, min: null, max: null });
+
+    useEffect(() => {
+        props.sensor.on(props.id, updateValues)
+
+        return () => {
+            props.sensor.off(props.id, updateValues);
+        }
+    }, [props.sensor, props.id]);
+
+    const updateValues = (value: number) => {
+        setState(({ min, max }) => {
+            return {
+                value: value,
+                min: min ? Math.min(min, value) : value,
+                max: max ? Math.max(max, value) : value
+            }
+        })
+    }
+
+    const onResetClick = () => {
+        setState(({ value }) => {
+            return {
+                value: value,
+                min: value,
+                max: value
+            }
+        })
+    }
+
+    return <div id={props.id} className='gauge'>
+        <h2>{props.id}</h2>
+        <Display id="value" label={props.id} value={state.value} unit={props.unit} />
+        <Display id="min" label="Min" value={state.min} unit={props.unit} />
+        <Display id="value" label="Max" value={state.max} unit={props.unit} />
+        <button className="gauge__reset button" onClick={onResetClick}>Reset</button>
+    </div>
+}
+
+export default Gauge;
